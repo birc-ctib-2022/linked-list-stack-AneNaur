c41fed8736626e4a9df918a0b0a12ae87566b44c
AneNaur <202008164@post.au.dk>
2022-11-29 12:03:26 +0100

    stack.py

--- src/stack.py
+++ src/stack.py
@@ -17,6 +17,8 @@ class Link(Generic[T]):
 
 List = Optional[Link[T]]
 
+class EmptyStack(Exception):
+    pass
 
 class Stack(Generic[T]):
     """A stack of elements of (generic) type T."""
@@ -24,19 +26,33 @@ class Stack(Generic[T]):
     def __init__(self) -> None:
         """Create a new stack of values of type T."""
         # FIXME: code here
+        self.stack = []
 
     def push(self, x: T) -> None:
         """Push x on the top of this stack."""
         # FIXME: code here
+        self.stack.append(x)
 
     def top(self) -> T:
         """Return the top of the stack."""
         # FIXME: code here
+        try:
+            return self.stack.get_last()
+        except IndexError:
+            raise EmptyStack()
 
     def pop(self) -> T:
         """Pop the top element off the stack and return it."""
         # FIXME: code here
+        try:
+            x = self.stack.get_last()
+            self.stack.remove_last()
+            return x
+        except IndexError
+            raise EmptyStack()
 
     def is_empty(self) -> bool:
         """Test if the stack is empty."""
         # FIXME: code here
+        return len(self.stack)==0
+
